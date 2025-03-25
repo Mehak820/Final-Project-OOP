e2be0e8c5d04e4ce79af1725471284551d70a71d
Mehak820 <mehakbrar343@gmail.com>
2025-03-25 13:17:01 -0600

    added comments

--- Final-Project-OOP.cs
+++ Final-Project-OOP.cs
@@ -10,7 +10,10 @@ namespace FinalProject
     internal class Program
     {
         static char[,] board = new char[6, 7];
+        // for connect four we will need 6 rows and 7 columns
         static char currentPlayer = 'X';
+        // player X will start the game 
+        // further should be written below or you can modify it as you wish
             static void Main(string[] args)
         {
         }
